f9ab8d14f0b4e2853a68c9800e2c8602de3aa221
kyle <kyjoconn@ucsc.edu>
2017-05-25 13:29:39 -0700

    displaying current location in longitude and latitude

--- app/src/main/java/com/kyjoconn/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/kyjoconn/myfirstapp/DisplayMessageActivity.java
@@ -1,16 +1,25 @@
 package com.kyjoconn.myfirstapp;
 
 import android.content.Intent;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DisplayMessageActivity extends AppCompatActivity {
+
+    private TrackGPS gps;
+    double longitude;
+    double latitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+
+
 
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
@@ -25,5 +34,21 @@ public class DisplayMessageActivity extends AppCompatActivity {
         // Capture the layout's TextView and set the string as its text
         TextView textView = (TextView) findViewById(R.id.textView);
         textView.setText(companyName + "\n" + street + "\n" + city + "\n" + state + "\n" + zip);
+
+        gps = new TrackGPS(DisplayMessageActivity.this);
+
+
+        if (gps.canGetLocation()) {
+
+
+            longitude = gps.getLongitude();
+            latitude = gps.getLatitude();
+
+
+        }
+
+        Toast.makeText(getApplicationContext(), "Longitude:" + Double.toString(longitude) + "\nLatitude:" + Double.toString(latitude), Toast.LENGTH_SHORT).show();
+        TextView distanceText = (TextView) findViewById(R.id.distanceText);
+        distanceText.setText("Longitude:" + Double.toString(longitude) + "\nLatitude:" + Double.toString(latitude));
     }
 }
