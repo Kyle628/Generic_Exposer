aeb58644fa7f50c21932e5446515f7a12ed809d5
kyle <kyjoconn@ucsc.edu>
2017-05-25 15:36:12 -0700

    correctly displaying current coordinates and plant coordinates

--- app/src/main/java/com/kyjoconn/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/kyjoconn/myfirstapp/DisplayMessageActivity.java
@@ -1,12 +1,20 @@
 package com.kyjoconn.myfirstapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
+import java.util.List;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -50,5 +58,37 @@ public class DisplayMessageActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), "Longitude:" + Double.toString(longitude) + "\nLatitude:" + Double.toString(latitude), Toast.LENGTH_SHORT).show();
         TextView distanceText = (TextView) findViewById(R.id.distanceText);
         distanceText.setText("Longitude:" + Double.toString(longitude) + "\nLatitude:" + Double.toString(latitude));
+
+        String address = street + " " + city + " " + state + " " + zip;
+        LatLng plantCoords = getLocationFromAddress(getApplicationContext(), address);
+
+        TextView plantText = (TextView) findViewById(R.id.plantText);
+        plantText.setText("Longitude:" + Double.toString(plantCoords.longitude) + "\nLatitude:" + Double.toString(plantCoords.latitude));
+    }
+
+    public LatLng getLocationFromAddress(Context context, String strAddress) {
+
+        Geocoder coder = new Geocoder(context);
+        List<Address> address;
+        LatLng p1 = null;
+
+        try {
+            // May throw an IOException
+            address = coder.getFromLocationName(strAddress, 5);
+            if (address == null) {
+                return null;
+            }
+            Address location = address.get(0);
+            location.getLatitude();
+            location.getLongitude();
+
+            p1 = new LatLng(location.getLatitude(), location.getLongitude() );
+
+        } catch (IOException ex) {
+
+            ex.printStackTrace();
+        }
+
+        return p1;
     }
 }
