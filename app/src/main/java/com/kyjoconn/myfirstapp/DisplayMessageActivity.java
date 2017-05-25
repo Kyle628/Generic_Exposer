cf0439de7ccf1753b899db13b5be038bf55fedda
kyle <kyjoconn@ucsc.edu>
2017-05-25 16:27:28 -0700

    gonna make a new branch that refactors original spaghetti code

--- app/src/main/java/com/kyjoconn/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/kyjoconn/myfirstapp/DisplayMessageActivity.java
@@ -55,8 +55,7 @@ public class DisplayMessageActivity extends AppCompatActivity {
 
 
         }
-
-        Toast.makeText(getApplicationContext(), "Longitude:" + Double.toString(longitude) + "\nLatitude:" + Double.toString(latitude), Toast.LENGTH_SHORT).show();
+        
         TextView distanceText = (TextView) findViewById(R.id.distanceText);
         distanceText.setText("Longitude:" + Double.toString(longitude) + "\nLatitude:" + Double.toString(latitude));
 
