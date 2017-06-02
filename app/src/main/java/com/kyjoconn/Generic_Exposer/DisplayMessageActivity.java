5fe8458ae3d91c35267a4ae861994ed558e9ce09
kyle <kyjoconn@ucsc.edu>
2017-06-02 18:46:20 -0400

    trying to modify csv's but they were in the gitignore

---
+++ app/src/main/java/com/kyjoconn/Generic_Exposer/DisplayMessageActivity.java
@@ -0,0 +1,112 @@
+package com.kyjoconn.Generic_Exposer;
+
+import android.content.Context;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
+import java.util.List;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    private TrackGPS gps;
+    double longitude;
+    double latitude;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        String companyName = extras.getString("companyName");
+        String street = extras.getString("street");
+        String city = extras.getString("city");
+        String state = extras.getString("state");
+        String zip = extras.getString("zip");
+        //String companyName = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(companyName + "\n" + street + "\n" + city + "\n" + state + "\n" + zip);
+
+        gps = new TrackGPS(DisplayMessageActivity.this);
+
+
+        if (gps.canGetLocation()) {
+
+
+            longitude = gps.getLongitude();
+            latitude = gps.getLatitude();
+
+
+        }
+
+        TextView distanceText = (TextView) findViewById(R.id.distanceText);
+        distanceText.setText("Longitude:" + Double.toString(longitude) + "\nLatitude:" + Double.toString(latitude));
+
+        String address = street + " " + city + " " + state + " " + zip;
+        LatLng plantCoords = getLocationFromAddress(getApplicationContext(), address);
+
+        TextView plantText = (TextView) findViewById(R.id.plantText);
+        plantText.setText("Longitude:" + Double.toString(plantCoords.longitude) + "\nLatitude:" + Double.toString(plantCoords.latitude));
+
+        Location locationA = new Location("me");
+
+        locationA.setLatitude(latitude);
+        locationA.setLongitude(longitude);
+
+        Location locationB = new Location("plant");
+
+        locationB.setLatitude(plantCoords.latitude);
+        locationB.setLongitude(plantCoords.longitude);
+
+        float distance = locationA.distanceTo(locationB);
+        double distance_d = Double.parseDouble(String.valueOf(distance));
+        double num_miles = getMiles((distance_d));
+        TextView differenceText = (TextView) findViewById(R.id.differenceText);
+        differenceText.setText(String.valueOf((int)num_miles) + " miles away");
+
+    }
+
+    public LatLng getLocationFromAddress(Context context, String strAddress) {
+
+        Geocoder coder = new Geocoder(context);
+        List<Address> address;
+        LatLng p1 = null;
+
+        try {
+            // May throw an IOException
+            address = coder.getFromLocationName(strAddress, 5);
+            if (address == null) {
+                return null;
+            }
+            Address location = address.get(0);
+            location.getLatitude();
+            location.getLongitude();
+
+            p1 = new LatLng(location.getLatitude(), location.getLongitude() );
+
+        } catch (IOException ex) {
+
+            ex.printStackTrace();
+        }
+
+        return p1;
+    }
+
+    double getMiles(double i) {
+        return i*0.000621371192;
+    }
+}
---
+++ app/src/main/java/com/kyjoconn/Generic_Exposer/MainActivity.java
@@ -0,0 +1,121 @@
+package com.kyjoconn.Generic_Exposer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MyActivity";
+    public static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    public static Hashtable<String, ArrayList<String>> plants = new Hashtable<String, ArrayList<String>>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //
+        add("mpi_directory1.csv");
+        add("mpi_directory2.csv");
+
+        Log.d(TAG, plants.get("P1042").get(0));
+
+
+    } // end of onCreate
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        Bundle extras = new Bundle();
+
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String plantId = editText.getText().toString();
+        plantId = plantId.toUpperCase().replace("-","");
+
+        String companyName = plants.get(plantId).get(0);
+        Log.d(TAG, companyName);
+        String street = plants.get(plantId).get(1);
+        String city = plants.get(plantId).get(2);
+        String state = plants.get(plantId).get(3);
+        String zip = plants.get(plantId).get(4);
+
+        extras.putString("companyName", companyName);
+        extras.putString("street", street);
+        extras.putString("city", city);
+        extras.putString("state", state);
+        extras.putString("zip", zip);
+
+        intent.putExtras(extras);
+        //intent.putExtra(EXTRA_MESSAGE, companyName);
+        startActivity(intent);
+    }
+
+    public void add(String filename) {
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+
+        try {
+            InputStreamReader is = new InputStreamReader(getAssets().open(filename));
+            br = new BufferedReader(is);
+            while ((line = br.readLine()) != null) {
+
+                String[] nums = {"1","2","3","4","5","6","7","8","9"};
+                boolean skipLine = true;
+                for (int i = 0; i < nums.length; i++) {
+                    if (nums[i].charAt(0) == line.charAt(1)) {
+                        skipLine = false;
+                    }
+                }
+
+                if (skipLine) {
+                    continue;
+                }
+
+                // use comma as separator
+                String[] company = line.split(cvsSplitBy);
+
+                String[] plant_numbers = company[0].split("\\+");
+                //System.out.println(Arrays.toString(plant_numbers));
+
+                ArrayList<String>  nameAndAddress = new ArrayList<String>();
+                nameAndAddress.add(company[1]);
+                nameAndAddress.add(company[2]);
+                nameAndAddress.add(company[3]);
+                nameAndAddress.add(company[4]);
+                nameAndAddress.add(company[5]);
+
+                for (int i = 0; i < plant_numbers.length; i++) {
+                    plants.put(plant_numbers[i].replace("-",""), nameAndAddress);
+                }
+
+
+                //System.out.println("Plant [code= " + company[0] + " , name=" + company[1] + "]");
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
